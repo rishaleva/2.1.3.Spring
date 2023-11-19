ab4df60668ab246dc7834b8167018450e2daad13
rishaleva <rishaleva@yandex.ru>
2023-11-19 17:47:35 +0300

    MainTest ok/the displayed time is identical

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,16 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
     }
+
 }
